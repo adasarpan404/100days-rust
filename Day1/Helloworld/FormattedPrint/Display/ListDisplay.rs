3d7f02fb57f03bfab48c48367d6df492a8e4f4ae
Arpan Das <dasarpan471@gmail.com>
2023-01-22 12:23:54 +0530

    completed formatted print in rust

--- Day1/Helloworld/FormattedPrint/Display/ListDisplay.rs
+++ Day1/Helloworld/FormattedPrint/Display/ListDisplay.rs
@@ -1,1 +1,52 @@
-use std::fmt;
+use std::fmt; // importing fmt module
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+// impl fmt::Display for List {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // Extract the value using tuple indexing,
+//         // and create a reference to `vec`.
+
+//         let vec = &self.0;
+
+//         // Iterate over `v` in `vec` while enumerating the iteration
+//         // count in `count`.
+
+//         write!(f, "[")?;
+
+//         for (count, v) in vec.iter().enumerate() {
+//             // For every element except the first, add a comma.
+//             // Use the ? operator to return on errors.
+//             if count != 0 {
+//                 write!(f, ", ")?;
+//             }
+
+//             write!(f, "{}", v)?;
+//         }
+//         // closing the opening bracket and returning the result
+//         write!(f, "]")
+//     }
+// }
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: ", count)?;
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    // [0: 1, 1: 2, 2: 3]
+}
