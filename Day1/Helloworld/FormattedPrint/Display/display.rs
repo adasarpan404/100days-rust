71ea502a22da30c6f78962c8ae502e6931152df7
Arpan Das <dasarpan471@gmail.com>
2023-01-22 12:08:22 +0530

    i am learning formatted print in rust

--- Day1/Helloworld/FormattedPrint/Display/display.rs
+++ Day1/Helloworld/FormattedPrint/Display/display.rs
@@ -13,24 +13,61 @@ impl fmt::Display for MinMax {
     }
 }
 
-// // Define a structure where the fields are nameable for comparison.
-// #[derive(Debug)]
-// struct Point2D {
-//     x: f64,
-//     y: f64,
-// }
+// Define a structure where the fields are nameable for comparison.
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
 
-// // Similarly, implement `Display` for `Point2D`
-// impl fmt::Display for Point2D {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         // Customize so only `x` and `y` are denoted.
-//         write!(f, "x: {}, y: {}", self.x, self.y)
-//     }
-// }
+// Similarly, implement `Display` for `Point2D`
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+// completing assignment
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
 
 fn main() {
     let minmax = MinMax(0, 14);
     println!("Compare structures:");
     println!("Display: {}", minmax);
     println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // assigment
+    let complex = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    println!("Display and debug value for complex (assignment)");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", point);
 }
