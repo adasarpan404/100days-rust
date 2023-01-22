bee59ba34a35ff2f2f3a95dcc18e935bc05bdd1e
Arpan Das <dasarpan471@gmail.com>
2023-01-22 11:14:19 +0530

    i am learning formatted print in rust

--- Day1/Helloworld/formattedPrint.rs
+++ Day1/Helloworld/formattedPrint.rs
@@ -1,32 +1,40 @@
-fn main(){
+fn main() {
     // In general, the `{}` will be automatically replaced with any
     // arguments. These will be stringified.
     println!("{} days", 31);
-    
+
     // Positional arguments can be used. Specifying an integer inside `{}`
     // {0} is being replaced by Alice
     // {1} is being replaced by Bob
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
 
-    // I have used three arguments in it 
+    // I have used three arguments in it
     // {0} will be replaced by Alice
     // {1} will be replaced by Bob
     // {2} will be replaced by John
-    println!("{0} {1} {2} || {2} {0} {1} || {1} {0} {2} || {2} {1} {0}", "Alice", "Bob", "John");
+    println!(
+        "{0} {1} {2} || {2} {0} {1} || {1} {0} {2} || {2} {1} {0}",
+        "Alice", "Bob", "John"
+    );
 
     // String can be used as an arguments
     // {Subject} will be replaced by Subject="The quick brown fox"
     // {Verb} will be replaced by Verb="jumps over"
     // {Object} will be replaced by Object="the lazy dog"
-    println!("{Subject} {Verb} {Object}", Object="the lazy dog", Verb="jumps over", Subject="The quick brown fox");
+    println!(
+        "{Subject} {Verb} {Object}",
+        Object = "the lazy dog",
+        Verb = "jumps over",
+        Subject = "The quick brown fox"
+    );
 
     // Different formatting can be invoked by specifying the format character after a
-    // {} it will print as it is 
+    // {} it will print as it is
     // {:b} it will converted into binary
     // {:0} it will converted into octal
     // {:x} it will converted into hexadecimal
     // {:X} it will also converted into hexadecimal where every alphabet is converted to uppercase
-    println!("Base 10:               {}",   69420); //69420
+    println!("Base 10:               {}", 69420); //69420
     println!("Base 2 (binary):       {:b}", 69420); //10000111100101100
     println!("Base 8 (octal):        {:o}", 69420); //207454
     println!("Base 16 (hexadecimal): {:x}", 69420); //10f2c
@@ -34,11 +42,16 @@ fn main(){
 
     // You can right-justify text with a specified width. This will
     // output "    1". (Four white spaces and a "1", for a total width of 5.)
-    println!("{number:>5}", number=1);
+    println!("{number:>5}", number = 1);
 
-    // this how can pad 0's and any number after print
-    println!("{number:0<5}", number=1);
+    // this how can pad 0's in back of 1
+    println!("{number:0<5}", number = 1);
 
     // this how can pad 0's in front of 1
-    println!("{number:0>5}", number=1);
+    println!("{number:0>5}", number = 1);
+
+    // you can use named arguments for specifying width
+    println!("{number:0>width$}", number = 1, width = 5);
+    println!("{number:0<width$}", number = 1, width = 5);
+    
 }
