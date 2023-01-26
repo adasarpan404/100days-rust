48b24fee73a5ad2dd7dd7ca210ee7610ce9ff5de
Arpan Das <dasarpan471@gmail.com>
2023-01-26 14:14:35 +0530

    variable binding started on day5

---
+++ Day5/VariableBindings/intro.rs
@@ -0,0 +1,17 @@
+// rust provides type safety using static typing
+// Variable bindings can be type annotated when declared.
+
+fn main() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    
+}
